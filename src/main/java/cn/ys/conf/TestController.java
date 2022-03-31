d1052c153dd825cd515666a786d8c376a0f8714e
chAo <wxc075115@163.com>
2022-03-31 14:41:56 +0800

    dev 第一次提交

--- src/main/java/cn/ys/conf/TestController.java
+++ src/main/java/cn/ys/conf/TestController.java
@@ -3,5 +3,6 @@ package cn.ys.conf;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git dev");
     }
 }
